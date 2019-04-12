538b523dfa28b07669dff6bd48600a860e1623c5
Jonathan Kilner dos Santos Silva <jkss@windows.cin.ufpe.br>
2019-04-12 12:30:10 -0300

    Finalização da Q2

--- AP/AP3/Q2.hs
+++ AP/AP3/Q2.hs
@@ -5,14 +5,16 @@ data Command = Forward Int | Backward Int | TurnLeft | TurnRight
 data Direction = ToNorth | ToSouth | ToWest | ToEast
                 deriving (Eq, Show)
 
-
 destination :: (Int, Int) -> [Command] -> (Int, Int)
 
-destination x [] = x
+destination x y = dest x y ToNorth
 
-destination x (a:as) =  
+dest :: (Int, Int) -> [Command] -> Direction -> (Int, Int)
 
+dest x [] _ = x
 
+dest x (a:as) dir | a == TurnLeft || a == TurnRight = dest x as (getPosition a dir)
+                  | otherwise = dest (move x (getCommand a) dir) as dir
 
 move :: (Int, Int) -> Int -> Direction -> (Int, Int)
 
@@ -26,7 +28,6 @@ getPosition :: Command -> Direction -> Direction
 getPosition x y | x == TurnLeft  = intToPosition (mod ((positionToInt y) + 1) 4)
                 | x == TurnRight = intToPosition (mod (abs((positionToInt y) - 1)) 4)
 
-
 positionToInt :: Direction -> Int
 
 positionToInt ToNorth = 0
@@ -37,7 +38,7 @@ positionToInt ToEast = 3
 getCommand :: Command -> Int
 
 getCommand (Forward x) = x
-getCommand (Backward x) = x
+getCommand (Backward x) = -x
 
 intToPosition :: Int -> Direction
 
