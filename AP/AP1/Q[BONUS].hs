94ed9ba81ba36ed81c649ce420a671f1f9df6654
Jonathan Kilner dos Santos Silva <jkss@WINDOWS.CIN.UFPE.BR>
2019-04-22 11:57:04 -0300

    Teminado a AP1 e começado a resolução da prova de 2018.2

--- AP/AP1/Q[BONUS].hs
+++ AP/AP1/Q[BONUS].hs
@@ -9,14 +9,23 @@ processBankOperations :: [Double] -> [(Int, Int, Int, Double)] -> [Double]
 
 processBankOperations x [] = x
 
-processBankOperations x ((0, co, cd, val) : ops) =  processBankOperations (replace x co ( (x!!co) + val)) ops
+processBankOperations x ((0, co, cd, val) : ops) =  processBankOperations (replace x co ( (x !! co) + val)) ops
 
--- processBankOperations (a:as) [(1, co, cd, val) : ops] = 
+processBankOperations x ((1, co, cd, val) : ops) = processBankOperations a ops
+                                                    where 
+                                                        a = if ((x !! co) - val) >= 0
+                                                            then replace x co ( (x !! co) - val)
+                                                            else x
 
--- processBankOperations (a:as) [(2, co, cd, val) : ops] = 
+processBankOperations x ((2, co, cd, val) : ops) = processBankOperations a ops
+                                                    where 
+                                                        a = if ( (x !! co) - val) >= 0 
+                                                            then replace w cd ( (w !! cd) + val) 
+                                                            else x
+                                                        w = replace x co ( (x !! co) - val)
 
 
-
+--Substitui o n-ésimo termo de uma lista
 replace :: [Double] -> Int -> Double -> [Double]
 replace (a:as) 0 rp = (rp:as)
 replace (a:as) n rp = a:(replace as (n-1) rp)
