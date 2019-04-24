8c1483acb052ce24101dd24247db2f1fb60051b3
KilnerJhow <jkss@cin.ufpe.br>
2019-04-24 15:18:41 -0300

    Resolução das questões da prova de 2016_1 e remoção de arquivos duplicado

--- AP/AP2/Q2.hs
+++ AP/AP2/Q2.hs
@@ -1,14 +1,16 @@
-data Tree t = Nilt | Node t (Tree t) (Tree t)
+data IntTree t = Nilt | Node Int (IntTree t) (IntTree t)
     deriving (Show)
 
-isBST :: (Ord t) => Tree t -> Bool
+
+isBST :: IntTree t -> Bool
 
 isBST (Nilt) = True
-isBST (Node chave filhoEsq filhoDir) | (chave) > (key filhoEsq) = isBST filhoEsq
-                                     | (chave) < (key filhoDir) = isBST filhoDir
+isBST (Node _ Nilt Nilt) = True
+isBST (Node chave filhoEsq filhoDir) | (chave) > (key filhoEsq) && (chave) < (key filhoDir) = isBST filhoEsq && isBST filhoDir
                                      | otherwise = False
+                                     
+key :: IntTree t -> Int
 
-key :: (Ord t) => Tree t -> t
-
-key (Nilt) = 
+key (Nilt) = 0
 key (Node chave filhoEsq filhoDir) =  chave
+    
